dace7485ad92e11d81fbcb738cf722a0b58014ef
Mobile Ads Developer Relations <mad-devrel-noreply@google.com>
2026-07-08 13:13:09 -0700

    Updating IronSource iOS adapter for release version 9.5.0.0.0
    PiperOrigin-RevId: 944661194

--- Package.swift
+++ Package.swift
@@ -28,7 +28,7 @@ let package = Package(
   dependencies: [
     .package(
       url: "https://github.com/ironsource-mobile/Unity-Mediation-iAds-Swift-Package.git",
-      exact: "9.4.2"
+      exact: "9.5.0"
     ),
     .package(
       url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git",
@@ -49,7 +49,7 @@ let package = Package(
       name: "IronSourceAdapter",
       url:
         "https://dl.google.com/googleadmobadssdk/mediation/ios/ironsource/IronSourceAdapter-9.5.0.0.0.zip",
-      checksum: "9bfff45f3c29353c39b311f6814dfa0c5663149933d5863380ad649df89e5057"
+      checksum: "22ffdd0c437a43dd5ab8738d3cfe459ab303846740fc581cbe2f96bf7c6907c3"
     ),
   ]
 )
